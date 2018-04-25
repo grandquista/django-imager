4d8149edb1eeb8c0030540d190cbd8c1cc85073f
Jay Adams <j.adams011@hotmail.com>
2018-04-25 16:21:16 -0700

    adding mkting text

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'multiselectfield',
+    # 'sorl.thumbnail',
     'imagersite',
     'imager_profile',
+    # 'imager_products',
     'bootstrap3',
 ]
 
@@ -129,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
 
 # Django Registration Settings
 ACCOUNT_ACTIVATION_DAYS = 1
