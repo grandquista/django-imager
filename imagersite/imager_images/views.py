e61fcb2f0ed659f3504e9e4d7aece3f2f2f1c4f5
Jay Adams <j.adams011@hotmail.com>
2018-04-30 08:51:54 -0700

    fixing album view and cover views so if cover is none will have a cover

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -5,6 +5,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 
+def _album_with_cover(photos, cover):
+    if cover is not None:
+        return set(photos) | {cover}
+    return photos
+
+
 def album_view(request, album_id=None):
     """Album View."""
     username = request.user.get_username()
@@ -18,12 +24,16 @@ def album_view(request, album_id=None):
 
     context = {
         'profile': profile,
-        'albums': [{'cover': album.cover, 'link': reverse('album',
-                    args=[album.id])} for album in albums],
-        'photos': [{'thumb': photo, 'link': reverse('photo', args=[photo.id])} for photo in photos]
+        'albums': albums,
+        'photos': photos,
     }
     if album_id:
-        context['album'] = Album.objects.filter(id=album_id)
+        # import pdb; pdb.set_trace()
+        album = get_object_or_404(Album, id=album_id)
+        # import pdb; pdb.set_trace()
+        print(album)
+        context["photos"] = _album_with_cover(Photo.objects.filter(album__id=album.id).filter(published='PUBLIC'), album.cover)
+        context["album"] = album
         return render(request, 'imager_images/album.html', context)
     return render(request, 'imager_images/albums.html', context)
 
