9fda741c05daff56953ea95e4095155b88851d30
Jay Adams <j.adams011@hotmail.com>
2018-04-30 21:10:28 -0700

    fixed tests

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -42,7 +42,7 @@ def album_view(request, album_id=None):
         # import pdb; pdb.set_trace()
         context["photos"] = _album_with_cover(_public_or_user(Photo, username).filter(album__id=album.id), album.cover)
         context["album"] = album
-        context["cover"] = album.cover or sample(context['photos'] +[None], 1)[0]
+        context["cover"] = album.cover or sample(list(context['photos']) + [None], 1)[0]
         return render(request, 'imager_images/album.html', context)
     return render(request, 'imager_images/albums.html', context)
 
