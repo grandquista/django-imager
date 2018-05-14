2b5c610ab0904a463582fa76d3528397098c55c5
Adam Grandquist <grandquista@gmail.com>
2018-05-13 18:09:27 -0700

    Organize settings.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-SASS_PROCESSOR_ENABLED = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,50 +18,49 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', None)
+EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
-EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', None)
-# EMAIL_USE_SSL = False
-# EMAIL_TIMEOUT = os.environ.get('EMAIL_TIMEOUT', None)
-# EMAIL_SSL_KEYFILE = os.environ.get('EMAIL_SSL_KEYFILE', None)
-# EMAIL_SSL_CERTFILE = os.environ.get('EMAIL_SSL_CERTFILE', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'false').lower().strip() != 'false'
+DEBUG = os.environ.get('DEBUG', 'false').lower().strip() not in ('false', '')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
+
 SASS_PRECISION = 8
+SASS_PROCESSOR_ENABLED = True
 
 # Application definition
 INSTALLED_APPS = [
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sessions',
     'django.contrib.staticfiles',
-    'multiselectfield',
-    'sorl.thumbnail',
-    'imagersite',
-    'imager_profile',
     'imager_images',
-    'bootstrap3',
+    'imager_profile',
+    'imagersite',
+    'multiselectfield',
     'sass_processor',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'imagersite.urls'
@@ -91,14 +89,14 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', ''),
+        'NAME': os.environ.get('DB_NAME', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'PORT': '5432',
         'TEST': {
             'NAME': 'test_imager'
-        }
+        },
+        'USER': os.environ.get('DB_USER', ''),
     }
 }
 
@@ -111,33 +109,30 @@ AUTH_PASSWORD_VALIDATORS = []
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     'sass_processor.finders.CssFinder',
 ]
 
 # Django Registration Settings
 ACCOUNT_ACTIVATION_DAYS = 1
 LOGIN_REDIRECT_URL = '/'
+
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
